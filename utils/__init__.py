e41db6b9fa33af9c355ef800fa0a100ed2437c64
lonewolf zlife <neb.dara222@gmail.com>
2025-12-07 14:47:30 +0700

    fix: moved folder mapping to utils

--- utils/__init__.py
+++ utils/__init__.py
@@ -3,12 +3,13 @@ Utility modules for Rice Disease Classification
 """
 from utils.mapping import KHMER_LABELS, DISPLAY_LABELS
 from utils.label_map import DISEASE_INFO, LABEL_TO_KHMER, LABEL_TO_DISPLAY
-
+from utils.folder_map import FOLDER_MAP
 __all__ = [
     "KHMER_LABELS",
     "DISPLAY_LABELS",
     "DISEASE_INFO",
     "LABEL_TO_KHMER",
     "LABEL_TO_DISPLAY",
+    "FOLDER_MAP"
 ]
 
